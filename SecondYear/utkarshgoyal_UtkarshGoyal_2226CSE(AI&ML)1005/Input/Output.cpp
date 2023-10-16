d63eca76ca4b8e89fdf2d503f1eb52810ffa746e
Utkarsh_Goyal <utkarshgoyal07@gmail.com>
2023-10-17 00:11:56 +0530

    this is my first program in contribution

---
+++ SecondYear/utkarshgoyal_UtkarshGoyal_2226CSE(AI&ML)1005/Input/Output.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	char c;
+	scanf("%c",&c);
+	if(c>='A'&&c<='Z')
+	printf("1");
+	else if(c>='a'&&c<='z')
+	printf("0");
+	else
+	printf("-1");
+	// Write your code here
+	
+}
